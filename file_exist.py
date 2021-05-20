f20319ac234163ecd59a888f0015175baf4a0d2f
tingyiwow <r07444005@ntu.edu.tw>
2021-05-20 11:08:31 +0800

    check file exist

---
+++ file_exist.py
@@ -0,0 +1,35 @@
+import os
+# 讀取檔案
+products = []
+if os.path.isfile('products.csv'): # 檢查檔案在不在
+	print('yeah!找到檔案了！')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+else:
+	print('找不到檔案.....')
+
+# 讀取檔案
+
+# 讓使用者輸入
+products = []
+while True:
+	name = input('請輸入商品名稱：')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格：')
+	products.append([name, price])
+print(products) 
+
+# 印出所有購買紀錄
+for p in products:
+	print(p[0], '的價格是', p[1])
+
+# 寫入檔案
+with open('product.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
